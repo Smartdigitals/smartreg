2267589e02fa5d5d682fc5049c99932199743af4
smart <smartsb516@gmail.com>
2023-05-02 22:01:40 +0100

    new files

---
+++ Procfile
@@ -0,0 +1,1 @@
+web gunicorn smartreg.wsgi:application --log-file -
---
+++ smartcode/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SmartcodeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'smartcode'
