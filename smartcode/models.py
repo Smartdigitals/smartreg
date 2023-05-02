9181b4be49c9aa0a674d29b32cc90bcdd068d532
smart <smartsb516@gmail.com>
2023-05-02 22:49:16 +0100

    superuser

--- smartcode/models.py
+++ smartcode/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Myreg(models.Model):
+    fname = models.CharField(max_length=200)
+    lname = models.CharField(max_length=200)
+    gender = models.CharField(max_length=200)
+    whatsapp = models.IntegerField()
+    location = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+
+    def __str__(self):
+        return self.fname
